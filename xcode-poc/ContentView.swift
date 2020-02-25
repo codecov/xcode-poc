9ccddab0f76769bd83a3b19a471796f56b4f50fd
Edward Izzo <edward@codecov.io>
2020-02-25 17:43:56 -0500

    Initial Commit

---
+++ xcode-poc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xcode-poc
+//
+//  Created by Edward Izzo on 2/25/20.
+//  Copyright © 2020 Codecov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
